59046f74e4f0ff559afb60d07347cf79b67d2ce0
DarioBosco <dario_bosco@protonmail.com>
2021-09-13 11:14:43 +0200

    Esercizio completato

--- src/js/main.js
+++ src/js/main.js
@@ -10,5 +10,19 @@ Vue.config.devtools = true;
 const app = new Vue({
 	el: '#app',
 	data: {
+		newToDo: '',
+		toDos: ['fare i compiti', 'fare la spesa', 'fare il bucato'],
+	},
+	methods: {
+		addToDo() {
+			if (this.newToDo != '') {
+				this.toDos.push(this.newToDo);
+				this.newToDo = '';
+			}
+		},
+		removeToDo(index) {
+			console.log(index);
+			this.toDos.splice(index, 1);
+		},
 	},
 });
